798f9d37988b90412e2dd4f9f5d942de09fc0c75
William Jackson <565174+williamjacksn@users.noreply.github.com>
2023-07-03 09:25:07 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-apk add --no-cache gcc mariadb-connector-c-dev musl-dev
+apk add --no-cache gcc mariadb-connector-c-dev musl-dev pkgconf
 
 # build
 pip wheel --no-deps --requirement requirements.txt
